9b30e0adb57e37bd7f7b50c473609bdd489033e7
kaleshivam019 <shivam.kale@gds.ey.com>
2022-07-05 10:47:16 +0530

    intial files

---
+++ src/main/java/com/ey/nwdashboard/NwDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ey.nwdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwDashboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwDashboardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/nwdashboard/NwDashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.nwdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwDashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
