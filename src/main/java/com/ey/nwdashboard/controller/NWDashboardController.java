7f317972590b3b8f22279e6fb84c361d3e8f752f
kaleshivam019 <shivam.kale@gds.ey.com>
2022-07-12 19:53:56 +0530

    CORS changes

--- src/main/java/com/ey/nwdashboard/controller/NWDashboardController.java
+++ src/main/java/com/ey/nwdashboard/controller/NWDashboardController.java
@@ -19,22 +19,25 @@ public class NWDashboardController {
     @Autowired
     VacationService vacationService;
 
+    @CrossOrigin(origins = {"http://localhost:3000"})
     @GetMapping(value = "dashboard/v1/users", produces = "application/json")
     public UserResponse getUsersOnLoad(){
         return userService.getUsersOnLoad();
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000"})
     @PostMapping(value = "dashboard/v1/add/user", consumes = "application/json", produces = "application/json")
     public ResponseEntity addNewUser(@RequestBody UserModel userModel){
         return userService.addNewUser(userModel);
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000"})
     @GetMapping(value = "dashboard/v1/{user-gpn}/vacations", produces = "application/json")
     public VacationResponse getUserVacations(@PathVariable("user-gpn") String userGPN){
         return vacationService.getUserVacations(userGPN);
     }
 
-    @CrossOrigin
+    @CrossOrigin(origins = {"http://localhost:3000"})
     @PostMapping(value = "dashboard/v1/add/vacations", consumes = "application/json", produces = "application/json")
     public ResponseEntity saveUserVacations(@RequestBody VacationRequest vacationRequest){
         return vacationService.saveUserVacations(vacationRequest);
