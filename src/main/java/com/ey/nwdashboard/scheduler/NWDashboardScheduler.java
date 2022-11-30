96f76a98fa1e909c85ed39368a576478e1e7ef5d
kaleshivam019 <shivam.kale@gds.ey.com>
2022-11-30 16:35:36 +0530

    Enabling scheduler to refresh the application

--- src/main/java/com/ey/nwdashboard/scheduler/NWDashboardScheduler.java
+++ src/main/java/com/ey/nwdashboard/scheduler/NWDashboardScheduler.java
@@ -1,19 +1,13 @@
 package com.ey.nwdashboard.scheduler;
 
-import com.ey.nwdashboard.entity.TrackerEntity;
-import com.ey.nwdashboard.entity.UserEntity;
 import com.ey.nwdashboard.model.MessageModelResponse;
-import com.ey.nwdashboard.service.impl.TrackerDBServiceImpl;
-import com.ey.nwdashboard.service.impl.UserDBServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.sql.Timestamp;
 import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,14 +22,14 @@ public class NWDashboardScheduler {
     /**
      * This scheduler is used to refresh the application every 30 mins to overcome the problem from idle/shutdown state in heroku
      */
-    /*@Scheduled(cron = "0 0/30 * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void refreshAppScheduler(){
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity <String> entity = new HttpEntity<>(headers);
 
-        restTemplate.exchange("https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/refresh", HttpMethod.GET, entity, String.class).getBody();
-    }*/
+        restTemplate.exchange("https://localhost:8080/dashboard/v1/refresh", HttpMethod.GET, entity, String.class).getBody();
+    }
 
     /**
      * This is the cron scheduler which will run on 1st day of every month at 12:02 AM
